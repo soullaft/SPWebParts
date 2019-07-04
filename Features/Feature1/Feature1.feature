576da08ff81eb09af46736f4ff324d5803d6f57e
Soullaft <soullaft@gmail.com>
2019-07-04 11:53:16 -0700

    main

---
+++ Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="83a17fac-5ce6-4669-84db-b4500fcd7915" featureId="83a17fac-5ce6-4669-84db-b4500fcd7915" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointProject2 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="9427653d-1093-4353-ab7b-a6c0aa94b898" />
+  </projectItems>
+</feature>
---
+++ TodoWorker/TodoWorkerUserControl.ascx.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SharePointProject2.TodoWorker
+{
+    public partial class TodoWorkerUserControl : UserControl
+    {
+        private readonly SPContext CurrentSite = SPContext.Current;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                FillData();
+            }
+        }
+
+        #region Events
+        /// <summary>
+        /// When value has changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void TasksList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //get selected index
+            int selectedIndex = (sender as DropDownList).SelectedIndex;
+            //get items depending on selected index
+            SPListItem item = CurrentSite.Web.Lists.TryGetList("TestList").Items[selectedIndex];
+            ClearTextBoxes();
+            //Fill textboxes
+            NameText.Text = item["Task Name"].ToString();
+
+            //If fields in tasks list is null(cause try catch is very heavy)
+            if (item["Start Date"] is DateTime)
+                StartDate.SelectedDate = (DateTime)item["Start Date"];
+
+            if (item["Due Date"] is DateTime)
+                DueDate.SelectedDate = (DateTime)item["Due Date"];
+
+            if (item["% Complete"] is Double)
+                CompleteBox.Text = item["% Complete"].ToString();
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = TasksList.SelectedIndex;
+            CurrentSite.Web.AllowUnsafeUpdates = true;
+            SPListItem item = CurrentSite.Web.Lists.TryGetList("TestList").Items[selectedIndex];
+
+            if (!string.IsNullOrEmpty(NameText.Text))
+                item["Task Name"] = NameText.Text;
+
+            item["Start Date"] = StartDate.SelectedDate;
+
+            item["Due Date"] = DueDate.SelectedDate;
+
+            if (!string.IsNullOrEmpty(CompleteBox.Text))
+                item["% Complete"] = double.Parse(CompleteBox.Text) * 100;
+
+            item.Update();
+
+            CurrentSite.Web.AllowUnsafeUpdates = false;
+
+            FillData();
+        }
+
+        /// <summary>
+        /// Fires when delete button was clicked
+        /// </summary>
+        /// <param name="sender"> Button </param>
+        /// <param name="e"></param>
+        protected void DeleteButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = TasksList.SelectedIndex;
+            //get items depending on selected index
+            SPListItem item = CurrentSite.Web.Lists.TryGetList("TestList").Items[selectedIndex];
+
+            item.Delete();
+
+            FillData();
+        }
+
+        #endregion
+
+        #region Helpers
+
+        protected void FillData()
+        {
+            //Bind List Items to Tasks List DropDown Control by id
+            TasksList.DataSource = CurrentSite.Web.Lists.TryGetList("TestList").Items;
+            TasksList.DataValueField = "Title";
+            TasksList.DataTextField = "Title";
+            TasksList.DataBind();
+
+            SPListItem item = CurrentSite.Web.Lists.TryGetList("TestList").Items[0];
+            ClearTextBoxes();
+
+            //Fill textboxes
+            NameText.Text = item["Task Name"].ToString();
+
+            //If fields in tasks list is null(cause try catch is very heavy)
+            if (item["Start Date"] is DateTime)
+                StartDate.SelectedDate = (DateTime)item["Start Date"];
+
+            if (item["Due Date"] is DateTime)
+                DueDate.SelectedDate = (DateTime)item["Due Date"];
+
+            if (item["% Complete"] is Double)
+                CompleteBox.Text = item["% Complete"].ToString();
+        }
+
+        /// <summary>
+        /// Hide controls
+        /// </summary>
+        /// <param name="hide">If <see cref="true"/> </param>
+        protected void HideShowControls(bool hide)
+        {
+
+        }
+
+        /// <summary>
+        /// Clear textboxes
+        /// </summary>
+        protected void ClearTextBoxes()
+        {
+            NameText.Text = "";
+            StartDate.ClearSelection();
+            DueDate.ClearSelection();
+            CompleteBox.Text = "";
+        }
+
+        #endregion
+    }
+}
